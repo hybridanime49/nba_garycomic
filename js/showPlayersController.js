6cd1c32e92d0ea28c25d2853a29dab39da50a7c5
hybridanime49 <hybridanime49@yahoo.com>
2016-01-05 00:24:45 -0800

    adding CSS animation from bootstrap

--- js/showPlayersController.js
+++ js/showPlayersController.js
@@ -1,15 +1,16 @@
 app.controller('showPlayersController', ['$scope', '$http', function($scope,$http){
 	
-	load_pictures();
+	//load_pictures();
 	// $interval(function(){
 	// 	load_pictures();
 	// },300);
-
-	function load_pictures(){
+	$scope.loadImages = function(){
 		$http.get('/load').success(function(data){
 			$scope.player_profiles=data;
 			console.log($scope.player_profiles);
 		});
-	};
+	}
+	$scope.loadImages();
+	
 
 }]);
